f643e4a37809209f02166ea1b8bf35b246813656
Moha-A1 <11469@holbertonstudents.com>
2025-06-17 15:28:52 +0200

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,7 +1,7 @@
 /**
 * main - Entry point
 *
-* Description: use sizeof 
+* Description: use sizeof
 *
 * Return: Always 0 (Success)
 */
@@ -15,6 +15,6 @@ printf("Sizeof of a float: %lu byte(s)\n", sizeof(float));
 printf("Sizeof of a int: %lu byte(s)\n", sizeof(int));
 printf("Sizeof of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
