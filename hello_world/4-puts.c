af187e007d978447486a4c5f8681addc65b9ba15
Moha-A1 <11469@holbertonstudents.com>
2025-06-17 13:56:05 +0200

    command:puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description : Cette fonction affiche "Programming is like building a multilingual puzzle sur la sortie standard.
+ *
+ * Retour : Toujours 0 (succès)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
