95530a073c54df34df58aa1640e6814ace8a92b0
Moha-A1 <11469@holbertonstudents.com>
2025-06-17 14:25:30 +0200

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+* main - Entry point
+*
+* Description: Prints a message with printf
+*
+* Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
