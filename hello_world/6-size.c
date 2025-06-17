bef0e21b1ee0cfc665087a29fd5c9b0722b759b6
Moha-A1 <11469@holbertonstudents.com>
2025-06-17 15:22:51 +0200

    use sizeof

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+/**
+* main - Entry point
+*
+* Description: use sizeof 
+*
+* Return: Always 0 (Success)
+*/
+
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("Sizeof of a float: %lu byte(s)\n", sizeof(float));
+printf("Sizeof of a int: %lu byte(s)\n", sizeof(int));
+printf("Sizeof of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long: %lu byte(s)\n", sizeof(long long));
+return (0);
+}
