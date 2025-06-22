5a1ea47813a2fbaf2f5493e2bad23ec558f9c2b8
Moha-A1 <11469@holbertonstudents.com>
2025-06-22 20:55:43 +0200

    enfin

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
