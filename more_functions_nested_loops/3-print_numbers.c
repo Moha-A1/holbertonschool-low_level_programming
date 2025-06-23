ba9abfbf1a4c67fec170f020ab11a7067e70d2fd
Moha-A1 <11469@holbertonstudents.com>
2025-06-23 12:32:43 +0200

    num

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by newline
+ */
+void print_numbers(void)
+{
+char n;
+
+for (n = '0'; n <= '9'; n++)
+_putchar(n);
+_putchar('\n');
+}
