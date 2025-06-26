fc308878d0dd483fc9e7694832860ccc21c82191
Moha-A1 <11469@holbertonstudents.com>
2025-06-26 12:41:36 +0200

    len, *s

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string whose length is to be calculated.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+
+int len;
+len = 0;
+
+while (*s != '\0')
+{
+	s++;
+	len++;
+}
+return (len);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,6 +22,6 @@ int add(int a, int b);
 void print_to_98(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-
+int _strlen(char *s);
 
 #endif /* MAIN_H */
