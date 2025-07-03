091e099754abc41884efddc3eb22df87df9b73f4
Moha-A1 <11469@holbertonstudents.com>
2025-07-03 13:44:34 +0200

    Fix 8-24_hours.c and add 0-whatsmyname.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int hour;
 	int min;
 
-	for (hour = 0; hour < 24; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (min = 0; min < 60; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
