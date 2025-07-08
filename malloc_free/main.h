2fd8eeee677d8715adc076cd68690b687b10321e
Moha-A1 <11469@holbertonstudents.com>
2025-07-08 15:57:25 +0200

    Write a function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -58,6 +58,7 @@ int is_prime_recursive_check(int n, int divisor);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 
 
