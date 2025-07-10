0ab600e93940f53675c644e58c3bfa364fecd74a
Moha-A1 <11469@holbertonstudents.com>
2025-07-10 19:28:34 +0200

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+# ifndef DOG_H
+# define DOG_H
+/**
+ * struct dog - Définit la structure pour un chien
+ * @name: Le nom du chien
+ * @age: L'âge du chien
+ * @owner: Le propriétaire du chien
+ *
+ * Description: Cette structure définit un chien avec son nom, son âge
+ * et son propriétaire.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
