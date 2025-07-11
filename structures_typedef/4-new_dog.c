19f95c9a43023faa3e7b38394d27b3d795e39b33
Moha-A1 <11469@holbertonstudents.com>
2025-07-11 16:52:21 +0200

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,14 +1,50 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strlen - calcule la longueur d'une chaîne de caractères
+ * @s: la chaîne de caractères à mesurer
  *
- * Return: pointer to the new dog, or NULL if it fails
+ * Return: la longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copie une chaîne de caractères
+ * @dest: la destination
+ * @src: la source
+ *
+ * Return: le pointeur vers la destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - crée un nouveau chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Return: pointeur vers le nouveau chien, ou NULL si échec
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -18,22 +54,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_d == NULL)
 		return (NULL);
 
-	new_d->name = malloc(strlen(name) + 1);
+	new_d->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (new_d->name == NULL)
 	{
 		free(new_d);
 		return (NULL);
 	}
-	strcpy(new_d->name, name);
+	_strcpy(new_d->name, name);
 
-	new_d->owner = malloc(strlen(owner) + 1);
+	new_d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (new_d->owner == NULL)
 	{
 		free(new_d->name);
 		free(new_d);
 		return (NULL);
 	}
-	strcpy(new_d->owner, owner);
+	_strcpy(new_d->owner, owner);
 
 	new_d->age = age;
 
