3f2156cc1d9985eb757ea2abd89829d52c145f16
Moha-A1 <11469@holbertonstudents.com>
2025-07-28 18:25:31 +0200

    Write a function that returns the number of elements in a linked list_t list.

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,8 +18,8 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Function prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 int _putchar(char c);
 
 #endif /* LISTS_H */
