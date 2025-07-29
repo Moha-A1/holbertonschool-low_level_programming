49858e7ccd53e47fad86e80f441d757de947d207
Moha-A1 <11469@holbertonstudents.com>
2025-07-29 09:28:57 +0200

    projet 1 task 3-4

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
+ * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer to the head of the list
  * @str: string to be added
  *
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *current;
 	unsigned int len = 0;
 
 	while (str[len])
@@ -29,8 +30,19 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
 
 	return (new_node);
 }
