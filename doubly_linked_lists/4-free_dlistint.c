80ebc36ac017053828e646072e785fe45e1c291f
Moha-A1 <11469@holbertonstudents.com>
2025-07-30 14:43:21 +0200

    frees a dlistint_t

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *next;
+
+	/* Traverse the list and free each node */
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
